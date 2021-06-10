ffb56719345b8d9b48b1483fd9009b853503c39d
Thiago Mateus <thiagomateus123@hotmail.com>
2021-06-10 18:52:58 -0300

    tirando o review trash

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,7 +8,7 @@ import entities.TempoParaTerminar;
 public class Program {
 
 	public static void main(String[] args) {
-		Review review1 = new Review(1, "Excelente Trabalho feito pelatua Mãe", "O jogo está maravilhoso supimpa belissimo eu gostaria de ganhar uns dias de féria pfv cara ");
+		Review review1 = new Review(1, "Excelente Trabalho", "O jogo está maravilhoso supimpa belissimo");
 		TempoParaTerminar t1 = new TempoParaTerminar(2, 12, false);
 		Avaliacao av1 = new AvaliacaoEspecializada(1, review1, t1, 6.2, 8.65, 7.4, 9.0, 4.5, 6.3);
 		System.out.println(av1);
